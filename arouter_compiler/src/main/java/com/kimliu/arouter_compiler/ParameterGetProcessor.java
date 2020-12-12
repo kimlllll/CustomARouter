0dbfa791fce11539462d6e3ee4e10b2deaa23902
liuyuan <liuyuan@xiongmaitech.com>
2020-12-12 17:21:37 +0800

    第四次上传： 添加注解ParameterGet

--- arouter_compiler/src/main/java/com/kimliu/arouter_compiler/ParameterGetProcessor.java
+++ arouter_compiler/src/main/java/com/kimliu/arouter_compiler/ParameterGetProcessor.java
@@ -1,8 +1,13 @@
 package com.kimliu.arouter_compiler;
 
 import com.google.auto.service.AutoService;
+import com.kimliu.arouter_annotation.ParameterGet;
 import com.kimliu.arouter_compiler.config.ProcessorConfig;
+import com.kimliu.arouter_compiler.utils.ProcesserUtils;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -14,6 +19,7 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
 import javax.lang.model.util.Types;
@@ -35,6 +41,9 @@ public class ParameterGetProcessor extends AbstractProcessor {
     // 文件生成器 生成类等
     private Filer filer;
 
+    // 创建仓库 ： key ： Activity类  value ： 该类中所有被标注的Parameter的集合
+    private Map<TypeElement, List<Element>> tempParameterMap = new HashMap<>();
+
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
         super.init(processingEnv);
@@ -47,6 +56,25 @@ public class ParameterGetProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+        if(!ProcesserUtils.isEmpty(annotations)){
+            // 获取所有被 @ParameterGet 标记的Parameter
+            Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(ParameterGet.class);
+            if(!ProcesserUtils.isEmpty(elements)){
+                for (Element element : elements) {
+
+                    // 获取属性的父节点  属性的父节点是类节点 : 该属性所在的类
+                    TypeElement typeElement = (TypeElement) element.getEnclosingElement();
+                    if(tempParameterMap.containsKey(typeElement)){
+                        //
+                    }
+
+
+                }
+            }
+
+        }
+
         return false;
     }
 }
